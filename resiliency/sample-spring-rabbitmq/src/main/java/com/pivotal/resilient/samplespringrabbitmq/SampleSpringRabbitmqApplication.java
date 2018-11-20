5202b96138f531a4ce98178ade642b99d81aac29
Marcial Rosales <mrosales@pivotal.io>
2018-11-20 11:02:28 +0100

    Add Spring AMQP app to demonstrate resiliency patterns

---
+++ resiliency/sample-spring-rabbitmq/src/main/java/com/pivotal/resilient/samplespringrabbitmq/SampleSpringRabbitmqApplication.java
@@ -0,0 +1,12 @@
+package com.pivotal.resilient.samplespringrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringRabbitmqApplication.class, args);
+	}
+}
