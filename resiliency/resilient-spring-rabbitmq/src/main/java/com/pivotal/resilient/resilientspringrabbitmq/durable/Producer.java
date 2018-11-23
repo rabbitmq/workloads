fefb7ca07d1fba9dd6461e8d5c99a02abf2f39f1
Marcial Rosales <mrosales@pivotal.io>
2018-11-23 16:16:40 +0100

    Log failure attempts to produce messages

--- resiliency/resilient-spring-rabbitmq/src/main/java/com/pivotal/resilient/resilientspringrabbitmq/durable/Producer.java
+++ resiliency/resilient-spring-rabbitmq/src/main/java/com/pivotal/resilient/resilientspringrabbitmq/durable/Producer.java
@@ -1,21 +1,27 @@
 package com.pivotal.resilient.resilientspringrabbitmq.durable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Producer {
+    private Logger logger = LoggerFactory.getLogger(Producer.class);
 
     private @Autowired @Qualifier("templateForDurableProducer")
     RabbitTemplate template;
 
     @Scheduled(fixedRate = 5000)
     public void sendMessageUsingCachedChannel() {
-        template.send(MessageBuilder.withBody("hello".getBytes()).build());
+        try {
+            template.send(MessageBuilder.withBody("hello".getBytes()).build());
+        }catch(RuntimeException e) {
+            logger.warn("Failed to send message due to {}", e.getMessage());
+        }
     }
 }
