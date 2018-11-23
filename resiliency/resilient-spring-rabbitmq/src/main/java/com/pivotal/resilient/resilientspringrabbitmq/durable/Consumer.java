cfa37365a058ad5a8748cc2b3c29b2ba83972651
Marcial Rosales <mrosales@pivotal.io>
2018-11-23 16:17:24 +0100

    Consumer survives in case of authentication failures

--- resiliency/resilient-spring-rabbitmq/src/main/java/com/pivotal/resilient/resilientspringrabbitmq/durable/Consumer.java
+++ resiliency/resilient-spring-rabbitmq/src/main/java/com/pivotal/resilient/resilientspringrabbitmq/durable/Consumer.java
@@ -5,12 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Consumer {
     private Logger logger = LoggerFactory.getLogger(Consumer.class);
+
+    @Value("${durable-consumer.possibleAuthenticationFailureFatal:false}") boolean possibleAuthenticationFailureFatal;
 
     @Bean
     public SimpleMessageListenerContainer consumerOnDurableQueue(Queue queue, ConnectionFactory connectionFactory) {
@@ -20,6 +23,9 @@ public class Consumer {
         container.setConnectionFactory(connectionFactory);
         container.setQueueNames(queue.getName());
         container.setMessageListener(new PlainMessageListener("consumer-durable"));
+
+        // it set to true, it will fail nad not recover if we get access refused
+        container.setPossibleAuthenticationFailureFatal(possibleAuthenticationFailureFatal);
 
         return container;
     }
