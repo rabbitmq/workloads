f9f8ba2556860107588824c8b4e839bfd9b501ed
Marcial Rosales <mrosales@pivotal.io>
2018-11-23 14:55:20 +0100

    Skeleton app for spring amqp resilient app

---
+++ resiliency/resilient-spring-rabbitmq/src/main/java/com/pivotal/resilient/resilientspringrabbitmq/ResilientSpringRabbitmqApplication.java
@@ -0,0 +1,12 @@
+package com.pivotal.resilient.resilientspringrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResilientSpringRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResilientSpringRabbitmqApplication.class, args);
+	}
+}
