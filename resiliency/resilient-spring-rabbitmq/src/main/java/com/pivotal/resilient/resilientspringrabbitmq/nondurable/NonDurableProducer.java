1d91839ecbdb7b3c3d16a18447063a29a21ef645
Marcial Rosales <mrosales@pivotal.io>
2018-11-23 17:18:53 +0100

    Produce and consume message using non-durable exchange/queue
    Test the resiliency implications of using non-durable queues.
    This pair of non-durable consumer and producer can be enabled or
    disabled via configuration.

---
+++ resiliency/resilient-spring-rabbitmq/src/main/java/com/pivotal/resilient/resilientspringrabbitmq/nondurable/NonDurableProducer.java
@@ -0,0 +1,29 @@
+package com.pivotal.resilient.resilientspringrabbitmq.nondurable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnProperty(prefix = "non-durable.consumer", value = "enabled", matchIfMissing = true)
+public class NonDurableProducer {
+    private Logger logger = LoggerFactory.getLogger(NonDurableProducer.class);
+
+    private @Autowired @Qualifier("templateForNonDurableProducer")
+    RabbitTemplate template;
+
+    @Scheduled(fixedRate = 5000)
+    public void sendMessageUsingCachedChannel() {
+        try {
+            template.send(MessageBuilder.withBody("hello".getBytes()).build());
+        }catch(RuntimeException e) {
+            logger.warn("Failed to send message due to {}", e.getMessage());
+        }
+    }
+}
---
+++ resiliency/resilient-spring-rabbitmq/src/main/java/com/pivotal/resilient/resilientspringrabbitmq/nondurable/NonDurableResourcesConfiguration.java
@@ -0,0 +1,83 @@
+package com.pivotal.resilient.resilientspringrabbitmq.nondurable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(prefix = "non-durable", value = "enabled", matchIfMissing = true)
+public class NonDurableResourcesConfiguration {
+
+    private Logger logger = LoggerFactory.getLogger(NonDurableResourcesConfiguration.class);
+
+    @Value("${non-durable-consumer.queue:non-durable-q}") String queueName;
+    @Value("${non-durable-consumer.directExchange:non-durable-e}") String exchangeName;
+    @Value("${non-durable-consumer.routingKey:non-durable-q}") String routingKey;
+
+    @Bean("non-durable-consumer.queue")
+    public Queue consumerQueue() {
+        return QueueBuilder.nonDurable(queueName).build();
+    }
+
+    @Bean("non-durable-consumer.directExchange")
+    public Exchange consumerExchange() {
+        return ExchangeBuilder.directExchange(exchangeName).durable(false).build();
+    }
+
+    @Bean("non-durable-consumer.binding")
+    public Binding consumerBinding() {
+        return new Binding(queueName, Binding.DestinationType.QUEUE, exchangeName, routingKey, null);
+    }
+
+    @Bean
+    public RabbitTemplate templateForNonDurableProducer(ConnectionFactory connectionFactory) {
+        logger.info("Creating templateForDurableProducer ...");
+
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setRoutingKey(routingKey);
+        template.setExchange(exchangeName);
+
+        return template;
+    }
+
+
+}
+
+class PlainMessageListener implements MessageListener {
+
+    private Logger logger = LoggerFactory.getLogger(PlainMessageListener.class);
+
+    private String name;
+    private long receivedMessageCount;
+    private long failedMessageCount;
+
+
+    public PlainMessageListener(String name) {
+        this.name = name;
+    }
+    public PlainMessageListener() {
+        this("");
+    }
+
+
+    @Override
+    public void onMessage(Message message) {
+
+        logger.info("{}/{} received (#{}/#{}) from {}/{} ",
+                name,
+                Thread.currentThread().getId(),
+                receivedMessageCount,
+                failedMessageCount,
+                message.getMessageProperties().getConsumerQueue(),
+                message.getMessageProperties().getConsumerTag()
+
+        );
+        receivedMessageCount++;
+    }
+}
