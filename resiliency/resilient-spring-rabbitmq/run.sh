28cab7eaa2bef84b12a5b7138d684976f6c3f0cd
Marcial Rosales <mrosales@pivotal.io>
2019-06-25 11:45:02 +0200

    Launch application without ssl settings

--- resiliency/resilient-spring-rabbitmq/run.sh
+++ resiliency/resilient-spring-rabbitmq/run.sh
@@ -1,3 +1,4 @@
+
 export SPRING_PROFILES_ACTIVE=Cloud
 export VCAP_APPLICATION='{"application_name":demo}'
 export VCAP_SERVICES=$(cat src/main/resources/singleNode.json)
@@ -6,6 +7,6 @@ export VCAP_SERVICES=$(cat src/main/resources/singleNode.json)
 # JAVA_ARGS=" -Djavax.net.ssl.trustStoreType=PKCS12 -Djavax.net.ssl.trustStore=/tmp/docker-test/key-store.p12  -Djavax.net.ssl.trustStorePassword=roboconf"
 
 # Use these Java_ARgs to use trust-store.p12 which does have the server's CA certificate
-JAVA_ARGS=" -Djavax.net.ssl.trustStoreType=PKCS12 -Djavax.net.ssl.trustStore=/tmp/docker-test/trust-store.p12  -Djavax.net.ssl.trustStorePassword=roboconf"
+# JAVA_ARGS=" -Djavax.net.ssl.trustStoreType=PKCS12 -Djavax.net.ssl.trustStore=/tmp/docker-test/trust-store.p12  -Djavax.net.ssl.trustStorePassword=roboconf"
 
-java $JAVA_ARGS -jar target/resilient-spring-rabbitmq-0.0.1-SNAPSHOT.jar
+java  -jar target/resilient-spring-rabbitmq-0.0.1-SNAPSHOT.jar $@
