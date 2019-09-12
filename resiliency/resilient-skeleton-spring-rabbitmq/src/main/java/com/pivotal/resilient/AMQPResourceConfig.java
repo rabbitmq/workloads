4f4bd9b6dcdcb0873b7ee7d1b2c5f3ad5ecc5c91
Marcial Rosales <mrosales@pivotal.io>
2019-09-12 09:43:01 +0200

    Write less code thanks to spring amqp auto-configuration

--- resiliency/resilient-skeleton-spring-rabbitmq/src/main/java/com/pivotal/resilient/AMQPResourceConfig.java
+++ resiliency/resilient-skeleton-spring-rabbitmq/src/main/java/com/pivotal/resilient/AMQPResourceConfig.java
@@ -7,6 +7,8 @@ import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFacto
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,17 +28,20 @@ public class AMQPResourceConfig {
      * Creates a default RabbitTemplate bean configured with the *producer* connectionFactory built by {@link AMQPResourceConfig} class.
      * The template sends messages by default to *amp.direct* exchange using *hello* routing key.
      *
+     *  We cannot use the default RabbitTemplate provide by auto-configuration because that one would use the @Primary connectionFactory
      * @param connectionFactory
      * @return
      */
     @Bean
-    public RabbitTemplate template(@Qualifier("producer") ConnectionFactory connectionFactory) {
+    public RabbitTemplate template(@Qualifier("producer") ConnectionFactory connectionFactory,
+                                   @Value("${spring.rabbitmq.template.routing-key:hello}") String routingKey,
+                                   @Value("${spring.rabbitmq.template.exchange:amq.direct}") String exchange) {
         logger.info("Creating template ...{publisher-confirms:{}}", connectionFactory.isPublisherConfirms());
 
 
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setRoutingKey("hello");
-        template.setExchange("amq.direct");
+        template.setRoutingKey(routingKey);
+        template.setExchange(exchange);
 
         return template;
     }
@@ -48,12 +53,15 @@ public class AMQPResourceConfig {
      * @param connectionFactory
      * @return
      */
-    @Bean
+    //@Bean
+    // This bean is not necessary because the auto-configurations builds one configured from RabbitProperties with the @Primary connectionFactory which is the consumer
+    // We want a custom bean when we want to configure settings which are not in RabbitProperties such as RecoveryBackoff and similar ones
     public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory);
         factory.setConcurrentConsumers(3);
         factory.setMaxConcurrentConsumers(10);
+
         return factory;
     }
 
