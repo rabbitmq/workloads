cae95e1a93b50d5b006cfca3c5280ae11d7414b8
Marcial Rosales <mrosales@pivotal.io>
2020-10-22 17:18:34 +0200

    Use single unamed binder rather named binder

--- resiliency/resilient-spring-cloud-stream/fire-and-forget-producer/src/main/java/com/pivotal/resilient/ResilientSpringRabbitmqApplication.java
+++ resiliency/resilient-spring-cloud-stream/fire-and-forget-producer/src/main/java/com/pivotal/resilient/ResilientSpringRabbitmqApplication.java
@@ -1,12 +1,7 @@
 package com.pivotal.resilient;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.Cloud;
-import org.springframework.cloud.cloudfoundry.CloudFoundryConnector;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ResilientSpringRabbitmqApplication {
